42450fa2f2fa998e02e4511514908a248e4aa946
fjollaavdiu <43884050+fjollaavdiu@users.noreply.github.com>
2019-04-30 21:07:49 +0200

    Add files via upload

---
+++ UDP/UDP-Klienti.py
@@ -0,0 +1,32 @@
+import socket
+import sys 
+import select
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_socket.settimeout(1.0)
+#serverName = 'localhost';
+serverName = input("Jepe emrin e serverit:")
+#serverPort = 12000;
+Port = input("Shenoni portin:")
+serverPort = int(Port)
+addr = (serverName, serverPort)
+
+while True:
+    print("============================================================================")
+    var=input("Zgjedhni njeren nga metodat: \n IPADRESA\n NUMRIIPORTIT\n BASHKETINGELLORE\n PRINTIMI\n EMRIIKOMPJUTERIT\n KOHA\n LOJA\n FIBONACCI\n KONVERTIMI\n" +" Por ne rast se keni perfunduar mund te shtypni enter per mbyllje programi\n ")
+    print("============================================================================")
+    if not var:
+        print("Shenoni njeren nga metodat!")
+        continue
+    if var == "enter":
+        client_socket.close()
+        break
+    client_socket.sendto(var.encode(),addr)
+    try:
+        data, server = client_socket.recvfrom(1024)
+        data = data.decode('utf-8')
+        print(data)
+    except socket.timeout:
+        print('REQUEST TIMED OUT')
+print("============================================================================")
+    
